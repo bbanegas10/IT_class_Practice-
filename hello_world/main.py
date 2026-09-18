a30a904aa48514839830b164cb5b354cd5d7a73d
Bayron Banegas <bayronbanegas@10-3-22-115.dhcp.marymount.edu>
2026-09-17 20:17:31 -0400

    Initial commit.

---
+++ hello_world/main.py
@@ -0,0 +1,17 @@
+print('Hello World!')
+# So does this one, but this is a function definition
+
+import hello_world
+
+# So does this one, but this is a function definition
+
+def main() :
+    #message = hello_world.get_message_from_user()
+    #hello_world.print_message(message)
+    number = hello_world.get_number_from_user()
+    print(f'User entered: {number}')
+    print('User entered: ' + str(number))
+
+
+if __name__ == "__main__":
+    main()
